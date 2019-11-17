34eaf079616ece16b9bcd7ec5cfb6813c96fc2ed
yoann2968 <yoann.spriet@gmail.com>
2019-11-17 14:04:53 +0100

    ajout d'un anneau pour position curseur

--- VISU/index.js
+++ VISU/index.js
@@ -37,12 +37,24 @@ function init() {
     scene = new THREE.Scene();
     enregistrerDansAnnuaire("scene", scene);
 
-    camera = new THREE.PerspectiveCamera(70.0, window.innerWidth / window.innerHeight, 0.1, 100.0);
+    camera = new THREE.PerspectiveCamera(70.0, window.innerWidth / window.innerHeight, 0.1, 10000.0);
     camera.position.set(0, 1.7, 5.0);
     camera.lookAt(new THREE.Vector3(0.0, 1.7, 0.0));
 
     listener = new THREE.AudioListener();
     camera.add(listener);
+
+    // add a ring in the middle of screen (for a "pointer")
+    var reticle = new THREE.Mesh(
+        new THREE.RingBufferGeometry(0.005, 0.015, 32),
+        new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.DoubleSide })
+    );
+    reticle.position.z = -1;
+    reticle.lookAt(camera.position)
+
+    camera.add(reticle);
+    scene.add(camera);
+
 
     window.addEventListener('resize', function () {
         windowHalfX = window.innerWidth / 2.0;
